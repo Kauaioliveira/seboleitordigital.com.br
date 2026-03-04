5ec99e9e99775f0a65f2df2e6f44c94351437a9e
Kauai Oliveira braga <kauaioliveira@outlook.com>
2026-03-03 22:28:52 -0300

    Sua mensagem de commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.getElementById("darkMode");
+button.addEventListener("click", function() {
+    document.body.classList.toggle("dark");
+});
+const Duna = document.getElementById("Duna");
+Duna.addEventListener("click", function() {
+    alert("Você clicou!");
+});
+
+const CodigoLimpo = document.getElementById(CodigoLimpo);
