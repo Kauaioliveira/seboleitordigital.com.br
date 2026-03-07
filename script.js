1fdc2d048ee9097f75665aa795bd88312f0a5500
Kauai Oliveira braga <kauaioliveira@outlook.com>
2026-03-06 22:24:12 -0300

    Sua mensagem de commit

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-const button = document.getElementById("darkMode");
-button.addEventListener("click", function() {
-    document.body.classList.toggle("dark");
-});
-const Duna = document.getElementById("Duna");
-Duna.addEventListener("click", function() {
-    alert("Você clicou!");
+//pega todos os livros (divs com classe "item")
+const itens = document.querySelectorAll('.item');
+//pega o parágrafo onde mostra a descrção
+const textoDescricao = document.getElementById("textoDescricao")
+//para cada livro, adiciona um "ouvinte" de clique.
+itens.forEach((item) => {
+    item.addEventListener('click', () => {
+        //lê o atributo data-descricao do livro clicado
+        const descricao = item.getAttribute('data-descricao');
+        //troca o texto na área de detalhes
+        textoDescricao.textContent = descricao;
+
+    })
 });
 
-const CodigoLimpo = document.getElementById(CodigoLimpo);
+//Dark Mode
+const botaoDark = document.getElementById('dark');
+
+//carregar preferencia salva do usuario ao abrir  a pagina
+if (localStorage.getItem('tema')=== 'dark') {
+    document.body.classList.add('dark');
+    botaoDark.textContent = 'light Mode';
+}
+
+botaoDark.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    if (document.body.classList.contains('dark')) {
+        localStorage.setItem('tema','dark');
+        botaoDark.textContent = 'Dark Mode'
+    }
+});
